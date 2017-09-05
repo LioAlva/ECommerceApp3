c849d700fe09917a59bf8e8a6d93224d37a2ba3d
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-09-05 12:10:09 -0500

    Ecommerce 103 Navegacion implementado

---
+++ ECommerceApp3/ECommerceApp3/ECommerceApp3/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+using ECommerceApp3.Pages;
+using Xamarin.Forms;
+
+namespace ECommerceApp3
+{
+    public partial class App : Application
+    {
+        public static NavigationPage Navigator { get; internal set; }
+     
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MasterPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ECommerceApp3/ECommerceApp3/ECommerceApp3/Services/NavigationService.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceApp3.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp3.Services
+{
+    public class NavigationService
+    {
+        public async Task Navigate(string pageName)
+        {
+            switch (pageName)
+            {
+                case "CustomersPage":
+                    await App.Navigator.PushAsync(new CustomersPage());
+                    ; break;
+                case "DeliveriesPage":
+                    await App.Navigator.PushAsync(new DeliveriesPage());
+                    ; break;
+                case "ProductsPage":
+                    await App.Navigator.PushAsync(new ProductsPage());
+                    ; break;
+                case "SetupPage":
+                    await App.Navigator.PushAsync(new SetupPage());
+                    ; break;
+                case "SyncPage":
+                    await App.Navigator.PushAsync(new SyncPage());
+                    ; break;
+                case "UserPage":
+                    await App.Navigator.PushAsync(new UserPage());
+                    ; break;
+                default:
+                    break;
+            }
+        }
+    }
+}
